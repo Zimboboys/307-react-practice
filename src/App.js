69589555d88cdf2226c5e92dcf1189fbcbbfedc9
Ethan Zimbelman <itszimbo@gmail.com>
2021-01-16 00:55:46 -0800

    basic data fetching/posting with server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Table from './Table';
 import Form from './Form';
 
 const App = () => {
   const [characters, updateCharacters] = useState([]);
 
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => updateCharacters(data.users_list))
+    .catch(err => console.error(err));
+  }, []);
+
+  const postCharacter = character => {
+    return fetch('http://localhost:5000/users/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(character)
+      })
+      .then(res => {
+        return res.status === 201;
+      })
+      .catch(err => {
+        console.error(err);
+        return false;
+      });
+  };
+
   const addCharacter = character => {
-    updateCharacters([...characters, character]);
+    postCharacter(character)
+    .then(status => {
+      if (status) updateCharacters([...characters, character]);
+    });
   };
 
   const removeCharacter = index => {
